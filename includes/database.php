d44e76ba4f96f992749e6682ded0dd6ec36b9a7a
zioan <zaharia.ioan@gmail.com>
2021-11-30 11:22:27 +0100

    simple database connection

---
+++ includes/database.php
@@ -0,0 +1,19 @@
+<?php
+
+// Params to connect to a database
+$dbHost = "localhost";
+$dbUser = "raw";
+$dbPass = "";
+$dbName = "phptutorial";
+
+//Connection to database
+$conn = mysqli_connect($dbHost, $dbUser, $dbPass, $dbName);
+
+//check if connection failed
+if($conn) {
+
+} else {
+  die("Database connection failed!");
+}
+
+?>
